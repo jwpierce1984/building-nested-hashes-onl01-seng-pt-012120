56755a8a3d3c2cb84d2a51cf360072506b377b0b
Justin Pierce <jwpierce1984@gmail.com>
2020-01-24 01:17:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,27 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+#def second_challenge
+#  epic_tragedy = {
+#    :montague => {
+#      #your key/value pairs here!
+#      }, 
+#    :capulet => {
+#      #your key/value pairs here!
+#      }}
 
   
+#end
+def first_challenge
+   epic_tragedy = {
+     Montague:{
+       patriarch:{}
+       matriarch:{},
+       hero:{},
+       hero_friends:[]
+     },
+     Capulet:{
+       patriarch:{}
+       matriarch:{},
+       heroine:{},
+       heroine_friends:[]
+     }
+   }
 end
